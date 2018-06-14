1538aa0376aa84f35fb53ee6774f8c0452c97a5b
julissa <rosmery0239@gmail.com>
2018-06-14 11:05:23 -0500

    travajando con funciones

---
+++ index.js
@@ -0,0 +1,26 @@
+/*var title = document.getElementById("title");
+console.log(title);
+console.log(title.firstChild);
+var linksList=document.getElementsByTagName("a");
+console.log(linksList);
+console.log(linksList[3]);
+console.log(linksList[3].firstChild);
+var tit=document.getElementById("tit");
+console.log(tit);
+console.log(tit.childNodes);
+console.log(tit.firstChild);
+console.log(tit.lastChild);*/
+
+var listas = document.getElementsByTagName("a");
+console.log(listas);
+console.log(listas[2]);
+console.log(listas[2].firstChild);
+
+var listItems=document.getElementsByClassName("item");
+console.log(listItems);
+console.log(listItems[0]);
+console.log(listItems[0].firstElementChild);
+console.log(listItems[0].firstElementChild.firstChild);
+console.log(listItems[2]);
+console.log(listItems[2].firstElementChild);
+console.log(listItems[2].firstElementChild.firstChild);
